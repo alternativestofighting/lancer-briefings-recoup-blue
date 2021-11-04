102bff135994f02e062c2b99d77d9170bd058353
alternativestofighting <93721801+alternativestofighting@users.noreply.github.com>
2021-11-04 10:36:20 -0700

    Update App.vue
    Customizing for our campaign

--- src/App.vue
+++ src/App.vue
@@ -101,56 +101,40 @@ export default {
       ],
       "pilots": [
         {
-          "callsign": "Jinzo",
-          "alias": "Shayan Arsenal",
-          "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Type - Worst Beat"
+          "callsign": "Geist",
+          "alias": "Dr. Simone Estelmist",
+          "code": "3b9ac473-5b0f-47ba-b5f9-d0e0519aedee//NDL-C-SATELLITE-TELLURION//3b9ac473-5b0f-47ba-b5f9-d0e0519aedee",
+          "corpro": "SSC",
+          "frame": "Dusk Wing",
+          "mech": "Being-in-Itself"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
-          "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Necrophile"
+          "callsign": "Rumble",
+          "alias": "Martin Wallace",
+          "code": "63022b7c-efaa-4eb8-82b9-556abe3ce05b//NDL-C-SIGMA-SEA//63022b7c-efaa-4eb8-82b9-556abe3ce05b",
+          "corpro": "IPS-N",
+          "frame": "Blackbeard",
+          "mech": "Nothing to Lose"
         },
         {
-          "callsign": "Starboard",
-          "alias": "Maria",
-          "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Scarlet Judicator"
-        },
-        {
-          "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
-          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Mayfly"
-        },
-        {
-          "callsign": "Compadre",
-          "alias": 'Clint "CC" Cruz',
-          "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Rio Bravo"
+          "callsign": "Torino",
+          "alias": "Vik Petrol",
+          "code": "ff728efa-1095-4e0e-bd6c-7fabf5d8ac84//NDL-C-DISCORDANT-MARCH//ff728efa-1095-4e0e-bd6c-7fabf5d8ac84",
+          "corpro": "IPS-N",
+          "frame": "Zheng",
+          "mech": "Jimmy"
         },
       ],
       "header": {
-        "planet": "Hercynia",
+        "planet": "[Classified]",
         "year": "5014u",
-        "system": "Ardennes-3",
-        "gate": "Atlas-Quanokrim",
-        "ring": "Atlas-Line",
-        "headerTitle": "Mirrorsmoke",
-        "headerSubtitle": "Mercenary Company",
-        "subheaderTitle": "Crisis Response",
-        "subheaderSubtitle": "Delta-Echo-Echo-Zulu",
+        "system": "[Classified]",
+        "gate": "[Classified]",
+        "ring": "[Classified]",
+        "headerTitle": "Project XENRS",
+        "headerSubtitle": "Lancer Wing",
+        "subheaderTitle": "Colony Defense",
+        "subheaderSubtitle": "Xray-Echo-November-Romeo-Sierra",
       },
       "options":{
         "eventsMarkdownPerMission": true
