42f4910455d7bdb0a0adfba39d30d42c4ccf9dc5
alternativestofighting <93721801+alternativestofighting@users.noreply.github.com>
2022-06-09 14:28:39 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -111,7 +111,7 @@ export default {
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "UN-A CLASSIFIED"
+          "mech": "Placeholder"
         },
         {
           "callsign": "Goldleaf",
